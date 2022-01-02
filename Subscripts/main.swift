8b2ddbd4b27e24048c98fc41afaaad2f78436da9
Lam.Nguyen <lam.nguyen@cognizant.com>
2022-01-02 18:41:22 -0500

    New

--- Subscripts/main.swift
+++ Subscripts/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Subscripts
+// Subscripts enable you to query instances of a type by writing one or more values in square brackets after the instance name
 
+// You use subscripts to set and retrieve values by index without needing separate methods for retrieval.
+
+// Syntax:
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[56])")
+
+// MARK: - Subscript Usage
+
+// Using subscripts to add another entry into a dictionary:
+
+var angels = [1: "cherubim", 2: "seraphim"]
+angels[3] = "ophanim"
+print(angels)
+
+// MARK: - Subscript Options
+// Subscripts can take any number of input parameters and these input parameters can be of any type.
+// Subscripts can also return a value of any type.
